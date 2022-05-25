6d7e0930e8e4909a03678024715ea96c2e1fd594
tako <inamototakumi@gmail.com>
2022-05-25 00:27:54 -0400

    first comment lmao

---
+++ War/first.py
@@ -0,0 +1,65 @@
+from hashlib import new
+import random
+suits = ('Hearts','Diamonds','Spades','Clubs')
+ranks = ('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack',
+         'Queen','King','Ace')
+
+
+values = {'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,
+          'Seven':7,'Eight':8,'Nine':9,'Ten':10,
+          'Jack':11,'Queen':12,'King':13,'Ace':14}
+
+
+class Card:
+    
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+        
+    def __str__(self):
+        return self.rank + " of " + self.suit
+    
+
+
+class Deck():
+    def __init__(self):
+        self.all_cards = []
+        
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit,rank)
+                
+                #Create the Card Object
+                self.all_cards.append(created_card)
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+        
+
+
+
+#two_hearts=Card('Hearts','Two')
+
+#three_of_clubs = Card('Clubs','Three')
+
+#print(two_hearts)
+
+#print(values[two_hearts.rank])
+
+#print(three_of_clubs.value)
+
+#print(two_hearts.value < three_of_clubs.value)
+
+new_deck = Deck()
+
+#print(new_deck.all_cards)
+
+'''first_card= new_deck.all_cards[0]
+bottom_card= new_deck.all_cards[-1]
+print(first_card)
+print(bottom_card)'''
+
+new_deck.shuffle()
+
+for card_object in new_deck.all_cards:
+    print (card_object)
