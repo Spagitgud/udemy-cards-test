cfd361579eaefecb4a4fb4ef3109530c601a4116
Spagitgud <93284918+Spagitgud@users.noreply.github.com>
2022-05-26 00:32:07 -0400

    Update first.py

--- War/first.py
+++ War/first.py
@@ -35,7 +35,28 @@ class Deck():
     def shuffle(self):
         random.shuffle(self.all_cards)
         
+    def deal_one(self):
+        return self.all_cards.pop() 
+        
+class Player():
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+        
+    def remove_one(self):
+        return self.all_cards.pop(0)
+    
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]:)
+    def __str__(self):
+        return f'Plater {self.name} has {len(self.all_cards)} cards.'
+    
 
+new_player = Player('tako')
+
+print(new_player)
+    
+    
 
 
 #two_hearts=Card('Hearts','Two')
