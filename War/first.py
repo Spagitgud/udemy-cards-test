3765127a806580667356bbafbb13f2ce6e6336a9
Spagitgud <93284918+Spagitgud@users.noreply.github.com>
2022-06-02 00:04:01 -0400

    Update first.py

--- War/first.py
+++ War/first.py
@@ -1,5 +1,7 @@
 from hashlib import new
 import random
+
+from setuptools import SetuptoolsDeprecationWarning
 suits = ('Hearts','Diamonds','Spades','Clubs')
 ranks = ('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack',
          'Queen','King','Ace')
@@ -47,14 +49,18 @@ class Player():
         return self.all_cards.pop(0)
     
     def add_cards(self,new_cards):
-        if type(new_cards) == type([]:)
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+            
     def __str__(self):
-        return f'Plater {self.name} has {len(self.all_cards)} cards.'
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
     
 
-new_player = Player('tako')
+#new_player = Player('tako')
 
-print(new_player)
+#print(new_player)
     
     
 
@@ -71,7 +77,7 @@ print(new_player)
 
 #print(two_hearts.value < three_of_clubs.value)
 
-new_deck = Deck()
+
 
 #print(new_deck.all_cards)
 
@@ -80,7 +86,49 @@ bottom_card= new_deck.all_cards[-1]
 print(first_card)
 print(bottom_card)'''
 
+'''new_deck = Deck()
 new_deck.shuffle()
 
 for card_object in new_deck.all_cards:
-    print (card_object)
+    print (card_object)'''
+    
+#GAME Setup
+player_one = Player("One")
+player_two = Player("Two")    
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True    
+round_num = 0
+
+while game_on:
+    round_num +=1
+    print(f'Round {round_num}')
+    
+    if len(player_one.all_cards) == 0:
+        print('Player One, out of cards! Player Two wins!')
+        game_on = False
+        break
+    if len(player_two.all_cards) == 0:
+        print ('Plater Two, out of cards!Player One Wins!')
+        game_on = False
+        break
+    
+    #Start a new Round!
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+    
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+    
+at_war = True
+
+while at_war:
+    
+#print(len(player_one.all_cards#))
